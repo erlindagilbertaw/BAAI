3201aa134545271d65904d13eac138721ef3435d
Erlinda Gilberta Wibawa <85277959+erlindagilbertaw@users.noreply.github.com>
2025-09-24 16:50:36 +0800

    UPDATE

---
+++ Erlinda_Minus.py
@@ -0,0 +1,11 @@
+#
+# Erlinda, 2025/09/24
+# File: Erlinda_Minus.py
+# Short description of the task
+#
+
+# 1. Input
+
+# 2. Process
+
+# 3. Output
---
+++ Erlinda_Sum.py
@@ -0,0 +1,15 @@
+#
+# Erlinda韋琳達, 2025/09/24
+# File: 0_template.py
+# Add two numbers then print
+#
+
+# 1. Input
+X = 8
+Y = 7
+
+# 2. Process
+Sum = X - Y
+
+# 3. Output
+print(f'Result {Sum}')
