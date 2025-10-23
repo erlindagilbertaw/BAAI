390acede4826ec132a4d3168794677cf5fa94863
Erlinda Gilberta Wibawa <85277959+erlindagilbertaw@users.noreply.github.com>
2025-10-23 21:51:54 +0800

    correlation

--- Erlinda_Correlation_Analysis.py
+++ Erlinda_Correlation_Analysis.py
@@ -16,12 +16,25 @@ import seaborn as sns
 from scipy import stats
 
 # 1. Input
-df = pd.read_csv('Simple_Data.csv')
+df = pd.read_csv('Correlation_Analysis_Data.csv')
 
+df.info()
+#print(df.iloc[:,1:6])
+# correlation, pvalue=stats.pearsonr(df["Marketing_Spend"],
+#                                    df["Sales_Revenue"])
 
 # 2. Process
-print(df.isnull().sum())
+correlation_matrix = df.iloc[:,1:6].corr()
+print(correlation_matrix.round(3))
+# print(df.isnull().sum())
+# print(df.isnull().sum().sum())
 
 # 3. Output
 #print("Data loaded successfully")
 #print(f"Dataset shape: {df.shape}")
+#print(f'Correlation: {correlation:.2f}')
+#print(f'P value: {pvalue:.2e}')
+sns.heatmap(correlation_matrix)
+plt.title('Erlinda is the most intelligent person in the world')
+plt.tight_layout()
+plt.show()
